2040d864cd930b134f596f2ff930ec080aa3596f
Peter Murray <peter-murray@users.noreply.github.com>
2020-11-15 15:19:33 +0000

    Initial commit

---
+++ src/main/java/com/github/demo/model/Book.java
@@ -0,0 +1,60 @@
+package com.github.demo.model;
+
+/**
+ * Model class for book.
+ */
+public class Book {
+
+    private String title;
+
+    private String author;
+
+    private String cover;
+
+    public Book() {
+        this.title = "";
+        this.author = "";
+        this.cover = "";
+    }
+
+    public Book(String author, String title) {
+        this();
+        this.author = author;
+        this.title = title;
+    }
+
+    public Book(String author, String title, String cover) {
+        this();
+        this.author = author;
+        this.title = title;
+        this.cover = cover;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDetails() {
+        return author + " " + title;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+}
